4c8013d19d18939fba23b30fce6a3f2ca0d55e85
irinebthayil <irinebthayil@gmail.com>
2023-05-27 05:03:32 +0530

    code

---
+++ src/App.js
@@ -0,0 +1,108 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import arrow from './assets/icon-arrow.svg'
+import mapboxgl from 'mapbox-gl'
+
+function App() {
+
+  const [ipdetails, setIPDetails] = useState({ ip: '', region: '', country: '', postalCode: '', timezone: ''})
+
+  const [location, setLocation] = useState([77.69557, 12.7111]);
+
+  useEffect(() => {
+    getIP();
+  }, []);
+
+  useEffect(() => {
+    mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_API_KEY;
+    const map = new mapboxgl.Map({
+      container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      zoom: 12, // starting zoom
+    });
+
+    map.flyTo({
+      center: location,
+      essential: true
+    });
+    const marker1 = new mapboxgl.Marker()
+      .setLngLat(location)
+      .addTo(map);
+  }, [location]);
+
+  function getIP(ip=''){
+    let url = '';
+    if(ip == '')
+    {
+      url = 'https://geo.ipify.org/api/v2/country,city?apiKey=' + process.env.REACT_APP_IPIFY_API_KEY
+    }
+    else {
+      url = 'https://geo.ipify.org/api/v2/country,city?apiKey=' + process.env.REACT_APP_IPIFY_API_KEY +'&ipAddress='+ip
+    }
+    
+    fetch(url)
+      .then((res) => res.json())
+      .then((json) => {
+        if(json.code == 422)
+        {
+          alert(json.messages);
+        }
+        else{
+          let dets = {}
+          dets.ip = json.ip;
+          dets.region = json.location.region;
+          dets.country = json.location.country;
+          dets.postalCode = json.location.postalCode;
+          dets.timezone = json.location.timezone;
+          dets.isp = json.isp;
+          setIPDetails(dets);
+          let loc = [json.location.lng, json.location.lat];
+          setLocation(loc);
+          console.log(json.location)
+        }
+        
+      })
+  }
+
+  function onSearchIP()
+  {
+    getIP(document.getElementById('searchIpInput').value);
+  }
+
+  return (
+    <div className="parent-div">
+      <div className='header-div'>
+      </div>
+      <div className='header-content'>
+        <h2>IP Address Tracker</h2>
+        <div className='searchbar'>
+          <input id='searchIpInput' type='text' placeholder='Search for any IP address or domain' />
+          <div className='arrow-div' onClick={onSearchIP}>
+            <img src={arrow} alt='Search' />
+          </div>
+        </div>
+        <div className='ip-details'>
+          <div className='details-maindiv'>
+            <p className='details-header'>IP ADDRESS</p>
+            <p className='details-content'>{ipdetails.ip}</p>
+          </div>
+          <div className='details-maindiv'>
+            <p className='details-header'>LOCATION</p>
+            <p className='details-content'>{ipdetails.region}, {ipdetails.country} {ipdetails.postalCode}</p>
+          </div>
+          <div className='details-maindiv'>
+            <p className='details-header'>TIMEZONE</p>
+            <p className='details-content'>UTC - {ipdetails.timezone}</p>
+          </div>
+          <div className='details-maindiv'>
+            <p className='details-header'>ISP</p>
+            <p className='details-content'>{ipdetails.isp}</p>
+          </div>
+        </div>
+      </div>
+      <div id="map"></div>
+    </div>
+  );
+}
+
+export default App;
